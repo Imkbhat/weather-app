c2c73ece635ecea2a5b99271faf2d30713d9ad0a
Imkbhat <kbs71190@gmail.com>
2020-08-03 19:20:39 +0530

    Updates

---
+++ src/components/Titles.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "../App.css";
+
+const Titles = () => (
+  <div>
+    <h1 className="title-container__title">Weather Finder</h1>
+    <h3 className="title-container__subtitle">
+      Find weather conditions and more..
+    </h3>
+  </div>
+);
+
+export default Titles;
